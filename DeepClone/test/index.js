0722b9cccd4a176f02f44cdad411a0f0e9f471bd
LarryKameZhao <larryzhao1994@outlook.com>
2020-12-08 07:44:46 +0800

    deep clone

---
+++ DeepClone/test/index.js
@@ -0,0 +1,111 @@
+const chai = require('chai');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+chai.use(sinonChai);
+
+const assert = chai.assert;
+const deepClone = require('../src/index');
+describe('deepClone', () => {
+  it('a function', () => {
+    assert.isFunction(deepClone);
+  });
+  it('能够复制基本类型', () => {
+    const n = 123;
+    const n2 = deepClone(n);
+    assert(n === n2);
+    const s = '12345';
+    const s2 = deepClone(s);
+    assert(s === s2);
+    const b = true;
+    const b2 = deepClone(b);
+    assert(b === b2);
+    const u = undefined;
+    const u2 = deepClone(u);
+    assert(u === u2);
+    const empty = null;
+    const empty2 = deepClone(empty);
+    assert(empty === empty2);
+    const sym = Symbol();
+    const sym2 = deepClone(sym);
+    assert(sym === sym2);
+  });
+  describe('对象', () => {
+    it('能够复制对象', () => {
+      const a = { name: 'sss', child: { name: 'sss-children' } };
+      const a2 = deepClone(a);
+      assert(a !== a2);
+      assert(a.name === a2.name);
+      assert(a.child !== a2.child);
+      assert(a.child.name === a2.child.name);
+    });
+    it('能够复制数组对象', () => {
+      const a = [
+        [11, 12],
+        [21, 22],
+        [31, 32],
+      ];
+      const a2 = deepClone(a);
+      assert(a !== a2);
+      assert(a[0] !== a2[0]);
+      assert(a[1] !== a2[1]);
+      assert(a[2] !== a2[2]);
+      assert.deepEqual(a, a2);
+    });
+    it('能够复制函数', () => {
+      const a = function (x, y) {
+        return x + y;
+      };
+      a.xxx = { yyy: { zzz: 1 } };
+      const a2 = deepClone(a);
+      assert(a !== a2);
+      assert(a.xxx !== a2.xxx);
+      assert(a.xxx.yyy !== a2.xxx.yyy);
+      assert(a.xxx.yyy.zzz === a2.xxx.yyy.zzz);
+      assert(a(1, 2) === a2(1, 2));
+    });
+    it('环也能复制', () => {
+      const a = { name: 'sss' };
+      a.self = a;
+      const a2 = deepClone(a);
+      assert(a !== a2);
+      assert(a.name === a2.name);
+      assert(a.self !== a2.self);
+    });
+    xit('不会爆栈', () => {
+      const a = { child: null };
+      let b = a;
+      for (let i = 0; i < 2000; i++) {
+        b.child = {
+          child: null,
+        };
+        b = b.child;
+      }
+      const a2 = deepClone(a);
+      assert(a !== a2);
+      assert(a.child !== a2.child);
+    });
+    it('可以复制正则', () => {
+      const a = /hi\d+/gi;
+      a.xxx = { yyy: { zzz: 1 } };
+      const a2 = deepClone(a);
+      assert(a.source === a2.source);
+      assert(a.flags === a2.flags);
+      assert(a !== a2);
+      assert(a.xxx !== a2.xxx);
+      assert(a.xxx.yyy !== a2.xxx.yyy);
+      assert(a.xxx.yyy.zzz === a2.xxx.yyy.zzz);
+    });
+    it('可以复制日期', () => {
+      const a = new Date();
+      a.xxx = { yyy: { zzz: 1 } };
+      const a2 = deepClone(a);
+      assert(a.source === a2.source);
+      assert(a.flags === a2.flags);
+      assert(a !== a2);
+      assert(a.getTime() === a2.getTime());
+      assert(a.xxx !== a2.xxx);
+      assert(a.xxx.yyy !== a2.xxx.yyy);
+      assert(a.xxx.yyy.zzz === a2.xxx.yyy.zzz);
+    });
+  });
+});
--- EventHub/src/index.ts
+++ EventHub/src/index.ts
@@ -1,17 +1,17 @@
 class EventHub {
-  cache = {};
+  cache: { [key: string]: Array<(data: unknown) => void> } = {};
 
   // {'报社1':[f1,f2,f3],'报社2':[f11,f22,f33]}
-  on(eventName, fn) {
+  on(eventName: string, fn: (data: unknown) => void) {
     // eventName = 报社1,   fn
     this.cache[eventName] = this.cache[eventName] || [];
     this.cache[eventName].push(fn);
   }
-  emit(eventName, data?) {
+  emit(eventName: string, data?: unknown) {
     (this.cache[eventName] || []).forEach((fn) => fn(data));
   }
 
-  off(eventName, fn) {
+  off(eventName: string, fn) {
     // 把fn从this.cache[eventName]中删除
     let index;
     this.cache[eventName] = this.cache[eventName] || [];
